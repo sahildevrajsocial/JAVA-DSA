56826bb01ae6b31429d46aad41eb1908919ad0dc
sahildevrajsocial <sdevrajsocial@gmail.com>
2026-05-16 20:49:08 +0530

    Initial testing

---
+++ src/tests/Testing.java
@@ -0,0 +1,15 @@
+package tests;
+
+public class Testing {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Devraj Java Tests!");
+
+        for (int i = 1; i <= 5; i++) {
+
+            System.out.println("Number: " + i);
+
+        }
+
+    }
+}
