d00895f6b2f274e11302bdd9fbf9889cd71db457
sahildevrajsocial <sdevrajsocial@gmail.com>
2026-05-29 17:35:06 +0530

    learned about Pattern 3

---
+++ com/devraj/patterns/Pattern3.java
@@ -0,0 +1,19 @@
+package com.devraj.patterns;
+
+import java.util.*;
+
+public class Pattern3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number:- ");
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
