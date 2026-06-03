22ac3cb019fd1815bde03bdebdc1a751d6fcdc91
sahildevrajsocial <sdevrajsocial@gmail.com>
2026-06-04 00:43:01 +0530

    learned about Pattern 21

---
+++ com/devraj/patterns/Pattern21.java
@@ -0,0 +1,28 @@
+package com.devraj.patterns;
+
+import java.util.Scanner;
+
+public class Pattern21 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number:- ");
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+            if(i == 1 || i == n ) {
+                for (int j = 1; j <= n; j++) {
+                    System.out.print("*");
+                }
+            }
+            else {
+                System.out.print("*");
+                for(int j = 1; j <= n - 2; j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
