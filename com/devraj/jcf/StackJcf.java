051f15ec6352c4c9599e7922ad64ca9bb098ff3b
sahildevrajsocial <sdevrajsocial@gmail.com>
2026-06-07 01:01:36 +0530

    learned about stack

---
+++ com/devraj/jcf/StackJcf.java
@@ -0,0 +1,17 @@
+package com.devraj.jcf;
+
+import java.util.*;
+
+public class StackJcf {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(2);
+        st.push(4);
+        st.push(6);
+        System.out.println(st);
+        System.out.println(st.peek());
+        st.pop();
+        System.out.println(st.peek());
+        System.out.println(st.isEmpty());
+    }
+}
