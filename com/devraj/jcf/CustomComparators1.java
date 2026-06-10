45506a65b3ffd07e0961324a93026429f26896f0
sahildevrajsocial <sdevrajsocial@gmail.com>
2026-06-11 01:34:01 +0530

    learned custom comparator to sort arraylist in descending order

---
+++ com/devraj/jcf/CustomComparators1.java
@@ -0,0 +1,29 @@
+package com.devraj.jcf;
+
+import java.util.*;
+
+public class CustomComparators1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<>();
+        al.add(1);
+        al.add(5);
+        al.add(4);
+        // sort it in the descending order\
+        Collections.sort(al, new Comparator<Integer>() {
+           // [num1, num2]
+           @Override
+           public int compare(Integer num1, Integer num2) {
+               // num1 < num2
+               // order is wrong
+               if(num1 < num2) {
+                   return 1;
+               }
+               else if(num1 > num2) {
+                   return -1;
+               }
+               return 0;
+           }
+        });
+        System.out.println(al);
+    }
+}
