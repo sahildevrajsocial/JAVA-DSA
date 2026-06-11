f535267c382a3be9c675d3d5c57e007148a0aa02
sahildevrajsocial <sdevrajsocial@gmail.com>
2026-06-11 19:23:57 +0530

    count digits of a number

---
+++ com/devraj/maths/CountDigits.java
@@ -0,0 +1,19 @@
+package com.devraj.maths;
+
+import java.util.*;
+
+public class CountDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number:- ");
+        int number = sc.nextInt();
+
+        if(number == 0) System.out.println(1);
+        int count = 0;
+        while(number > 0) {
+            number /= 10;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
