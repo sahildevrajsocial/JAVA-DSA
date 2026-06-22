ee154a300d0d5281907844df88c0200208284b10
sahildevrajsocial <sdevrajsocial@gmail.com>
2026-06-23 01:58:48 +0530

    learned to check if a string is palindrome or not

---
+++ com/devraj/strings/PalindromeCheck.java
@@ -0,0 +1,28 @@
+package com.devraj.strings;
+
+import java.util.Scanner;
+
+public class PalindromeCheck {
+
+    static boolean palindromeCheck(String s) {
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while(left < right) {
+            if(s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a String:- ");
+        String s = sc.nextLine();
+
+        System.out.println(palindromeCheck(s));
+    }
+}
