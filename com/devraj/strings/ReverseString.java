ab230638747ef37d3f9408a21acc1671cac6e777
sahildevrajsocial <sdevrajsocial@gmail.com>
2026-06-22 22:34:04 +0530

    reversed a string

---
+++ com/devraj/strings/ReverseString.java
@@ -0,0 +1,21 @@
+package com.devraj.strings;
+
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a String:- ");
+        String s = sc.nextLine();
+
+        int n = s.length();
+        char[] dup = new char[n];
+
+        for(int i = 0; i < s.length(); i++) {
+            dup[n - i - 1] = s.charAt(i);
+        }
+        String reversed = new String(dup);
+        System.out.println(reversed);
+    }
+}
